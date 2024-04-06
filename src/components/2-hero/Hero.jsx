d303d968315b92366b826ad347dd935d7ed58ed5
karimkashkoush <karimkashkoush5@gmail.com>
2024-04-06 21:16:16 +0200

    add

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -15,13 +15,14 @@ export default function Hero() {
                 <h2>front-end react developer</h2>
                 <p>Hi, I&apos;m Karim Mohamed Kashkoush, a passionate Front-End React Developer.</p>
                 <section className="social">
-                    <Link to=''><span className="icon-facebook"></span></Link>
-                    <Link to=''><span className="icon-instagram"></span></Link>
-                    <Link to=''><span className="icon-linkedin1"></span></Link>
-                    <Link to=''><span className="icon-x"></span></Link>
-                    <Link to=''><span className="icon-github"></span></Link>
+                    <Link to='http://wa.me/+201033113646' target='_blank'><span className="icon-whatsapp"></span></Link>
+                    <Link to='https://www.facebook.com/profile.php?id=100007925707362&mibextid=LQQJ4d' target='_blank'><span className="icon-facebook"></span></Link>
+                    <Link to='https://www.instagram.com/karimkashkoush1?igsh=b2poYWpmd2tra2Vi' target='_blank'><span className="icon-instagram"></span></Link>
+                    <Link to='https://www.linkedin.com/in/karim-kashkoush-47b27a1a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' target='_blank'><span className="icon-linkedin1"></span></Link>
+                    <Link to='https://x.com/Karimkashkoush_?t=8jJ6CfDXwhZPUUYUAkBnrA&s=09' target='_blank'><span className="icon-x"></span></Link>
+                    <Link to='https://github.com/KarimKashkoush' target='_blank'><span className="icon-github"></span></Link>
                 </section>
-                <a className='cv' href="/public/Karim-Mohamed-Kashkoush-CV.pdf" download>download cv <span className="icon-cloud-download"></span></a>
+                <a className='cv' href="https://drive.google.com/file/d/1iNvB3XwAods7lczZWYHnhteprQXbckJ9/view?usp=sharing" target='_blank'>download cv <span className="icon-cloud-download"></span></a>
             </section>
             <section className="animation-hero">
                 <Lottie animationData={personalAnimation} className="animation" />
--- src/components/4-projects/Projects.jsx
+++ src/components/4-projects/Projects.jsx
@@ -2,6 +2,28 @@ import { Link } from 'react-router-dom'
 import './projects.css'
 
 
+// @ts-ignore
+import p1 from '../../components/4-projects/P-1/p-1.png'
+// @ts-ignore
+import p2 from '../../components/4-projects/p-2/p-2.png'
+// @ts-ignore
+import p3 from '../../components/4-projects/p-3/p-3.png'
+// @ts-ignore
+import p4 from '../../components/4-projects/p-4/p-4.png'
+// @ts-ignore
+import p5 from '../../components/4-projects/P-5/p-5.png'
+// @ts-ignore
+import p6 from '../../components/4-projects/p-6/p6.png'
+// @ts-ignore
+import p7 from '../../components/4-projects/p-7/p-7.png'
+// @ts-ignore
+import p8 from '../../components/4-projects/p-8/p8.png'
+// @ts-ignore
+import p9 from '../../components/4-projects/p-9/p9.png'
+// @ts-ignore
+import p10 from '../../components/4-projects/p-10/p10.png'
+
+
 export default function Projects() {
     return (
         <section className="projects" id='projects'>
@@ -16,36 +38,120 @@ export default function Projects() {
                     <button>React & MUI</button>
                 </section>
                 <section className="all-projects">
-                    <Link to="/" className="project">
-                        <img src="/public/images/icons8-react-native.svg" />
+                    <Link to="https://leon-4zg4wkmj4-karimkashkoush.vercel.app/" className="project">
+                        <img src={p1} />
                         <section className="title">
                             <h4>project 1</h4>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque culpa iure laudantium?</p>
                             <section className="links">
-                                <Link to="" className='icon-link1'></Link>
-                                <Link to="" className='icon-github'></Link>
+                                <Link to="https://leon-4zg4wkmj4-karimkashkoush.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/Leon" className='icon-github'></Link>
                             </section>
                         </section>
                     </Link>
-                    <Link to="/" className="project">
-                        <img src="/public/images/icons8-react-native.svg" />
+                    <Link to="https://karim-9qa946dkd-karimkashkoush.vercel.app/" className="project">
+                        <img src={p2} />
                         <section className="title">
                             <h4>project 1</h4>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque culpa iure laudantium?</p>
                             <section className="links">
-                                <Link to="" className='icon-link1'></Link>
-                                <Link to="" className='icon-github'></Link>
+                                <Link to="https://karim-9qa946dkd-karimkashkoush.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/Karim" className='icon-github'></Link>
                             </section>
                         </section>
                     </Link>
-                    <Link to="/" className="project">
-                        <img src="/public/images/icons8-react-native.svg" />
+                    <Link to="https://kasper-hvz2fwlws-karimkashkoush.vercel.app/" className="project">
+                        <img src={p3} />
                         <section className="title">
                             <h4>project 1</h4>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque culpa iure laudantium?</p>
                             <section className="links">
-                                <Link to="" className='icon-link1'></Link>
-                                <Link to="" className='icon-github'></Link>
+                                <Link to="https://kasper-hvz2fwlws-karimkashkoush.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/Kasper" className='icon-github'></Link>
+                            </section>
+                        </section>
+                    </Link>
+
+                    <Link to="https://turbino-gym.vercel.app/" className="project">
+                        <img src={p4} />
+                        <section className="title">
+                            <h4></h4>
+                            <p></p>
+                            <section className="links">
+                                <Link to="https://turbino-gym.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/Turbino-Gym" className='icon-github'></Link>
+                            </section>
+                        </section>
+                    </Link>
+
+                    <Link to="https://steller-five.vercel.app/" className="project">
+                        <img src={p5} />
+                        <section className="title">
+                            <h4></h4>
+                            <p></p>
+                            <section className="links">
+                                <Link to="https://steller-five.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/Steller" className='icon-github'></Link>
+                            </section>
+                        </section>
+                    </Link>
+
+                    <Link to="https://farah-ashraf.vercel.app/" className="project">
+                        <img src={p6} />
+                        <section className="title">
+                            <h4></h4>
+                            <p></p>
+                            <section className="links">
+                                <Link to="https://farah-ashraf.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/Farah-Ashraf" className='icon-github'></Link>
+                            </section>
+                        </section>
+                    </Link>
+
+                    <Link to="https://d-sherif-abdelbaeth.vercel.app/" className="project">
+                        <img src={p7} />
+                        <section className="title">
+                            <h4></h4>
+                            <p></p>
+                            <section className="links">
+                                <Link to="https://d-sherif-abdelbaeth.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/D.Sherif.Abdelbaeth" className='icon-github'></Link>
+                            </section>
+                        </section>
+                    </Link>
+
+                    <Link to="https://spyro-gym-one.vercel.app/" className="project">
+                        <img src={p8} />
+                        <section className="title">
+                            <h4></h4>
+                            <p></p>
+                            <section className="links">
+                                <Link to="https://spyro-gym-one.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/Spyro-Gym" className='icon-github'></Link>
+                            </section>
+                        </section>
+                    </Link>
+
+                    <Link to="https://responsive-bakery.vercel.app/" className="project">
+                        <img src={p9} />
+                        <section className="title">
+                            <h4></h4>
+                            <p></p>
+                            <section className="links">
+                                <Link to="https://responsive-bakery.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/responsive-bakery" className='icon-github'></Link>
+                            </section>
+                        </section>
+                    </Link>
+
+                    <Link to="https://bsa-lake.vercel.app/" className="project">
+                        <img src={p10} />
+                        <section className="title">
+                            <h4></h4>
+                            <p></p>
+                            <section className="links">
+                                <Link to="https://bsa-lake.vercel.app/" className='icon-link1'></Link>
+                                <Link to="https://github.com/KarimKashkoush/BSA-Form" className='icon-github'></Link>
                             </section>
                         </section>
                     </Link>
