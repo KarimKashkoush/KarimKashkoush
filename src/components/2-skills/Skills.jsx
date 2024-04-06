934e28b75758b7d001fceae5c91b4d0c7e5d92c8
karimkashkoush <karimkashkoush5@gmail.com>
2024-04-06 16:24:24 +0200

    add

---
+++ src/components/2-skills/Skills.jsx
@@ -0,0 +1,49 @@
+import './skills.css';
+
+export default function Skills() {
+    return (
+        <section className="skills">
+            <h2 className="title-section">
+                Skills
+            </h2>
+            <section className="content">
+                <section className="skill">
+                    <img src="/public/images/icons8-html.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-css.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-sass.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-javascript.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-typescript.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-bootstrap.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-tailwind-css.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-react-native.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-material-ui.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-git.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-github.svg" />
+                </section>
+                <section className="skill">
+                    <img src="/public/images/icons8-firebase.svg" />
+                </section>
+            </section>
+        </section>
+    )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
 import Header from "../../components/1-header/Header"
-import Hero from "../../components/2-hero/Hero"
-import Main from "../../components/3-main/Main"
-import Contact from "../../components/4-contact/Contact"
-import Footer from "../../components/5-footer/Footer"
+import Skills from "../../components/2-skills/Skills"
+import Hero from "../../components/3-hero/Hero"
+import Main from "../../components/4-main/Main"
+import Contact from "../../components/5-contact/Contact"
+import Footer from "../../components/6-footer/Footer"
 
 
 export default function Home() {
@@ -11,6 +12,10 @@ export default function Home() {
             <Header />
             <section className="content">
                 <Hero />
+            </section>
+            <div className="line" />
+            <section className="content">
+                <Skills />
             </section>
             <div className="line" />
             <section className="content">
