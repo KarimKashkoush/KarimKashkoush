c5dda0a39bbd669558e4e5822b78861ea7560da8
karimkashkoush <karimkashkoush5@gmail.com>
2024-04-06 17:37:02 +0200

    add

--- src/components/6-footer/Footer.jsx
+++ src/components/6-footer/Footer.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom"
 import "./footer.css"
 
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <section className="footer">
+            <section className="links">
+                <ul>
+                    <li>
+                        <Link to='\'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='\'>Articles</Link>
+                    </li>
+                    <li>
+                        <Link to='\'>Speaking</Link>
+                    </li>
+                    <li>
+                        <Link to='\'>Uses</Link>
+                    </li>
+                </ul>
+            </section>
+            <section className="copy-rights">
+                <span>© {new Date().getFullYear()} Karim Kashkoush. All rights reserved.</span>
+            </section>
+        </section>
     )
 }
