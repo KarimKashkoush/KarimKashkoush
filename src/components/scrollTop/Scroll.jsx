aa10674431340c66ccbdd012416b0120d022019f
karimkashkoush <karimkashkoush5@gmail.com>
2024-04-07 12:55:43 +0200

    add

--- src/components/scrollTop/Scroll.jsx
+++ src/components/scrollTop/Scroll.jsx
@@ -1,33 +1,41 @@
 import './scroll.css'
-
+import { useEffect, useState } from 'react';
 
 export default function Scroll() {
 
-    let btn = document.getElementById("scroll");
-    window.onscroll = ()=>{
-        if(window.scrollY > 50) {
-            btn.style.display = 'block'
-        } else {
-            btn.style.display = 'none'
-        }
-    }
+    const [showButton, setShowButton] = useState(false)
 
-    if(window.scrollY > 100) {
-        btn.style.display = 'block'
-    } else {
-        btn.style.display = 'none'
-    }
 
-    function scrollTop() {
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollTop = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
-    }
+    };
+
     return (
-        <section className="scroll" id='scroll' onClick={()=>{scrollTop()}}>
-            <span className='icon-keyboard_arrow_up'></span>
-        </section>
+        <>
+            {showButton && (
+                <section className="scroll" id='scroll' onClick={() => { scrollTop() }}>
+                    <span className='icon-keyboard_arrow_up'></span>
+                </section>
+            )}
+        </>
     )
 }
 
