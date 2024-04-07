b9f26c39bc23e468102bbf5f1d8dd908c61c1874
karimkashkoush <karimkashkoush5@gmail.com>
2024-04-07 16:09:28 +0200

    add

--- src/components/4-projects/Projects.jsx
+++ src/components/4-projects/Projects.jsx
@@ -1,6 +1,26 @@
 import { Link } from 'react-router-dom'
 import './projects.css'
 
+// @ts-ignore
+import p1 from '../../components/4-projects/P-1/p-1.png'
+// @ts-ignore
+import p2 from '../../components/4-projects/p-2/p-2.png'
+// @ts-ignore
+import p3 from '../../components/4-projects/p-3/p-3.png'
+// @ts-ignore
+import p4 from '../../components/4-projects/p-4/p-4.png'
+// @ts-ignore
+import p5 from '../../components/4-projects/P-5/p-5.png'
+// @ts-ignore
+import p6 from '../../components/4-projects/p-6/p6.png'
+// @ts-ignore
+import p7 from '../../components/4-projects/p-7/p-7.png'
+// @ts-ignore
+import p8 from '../../components/4-projects/p-8/p8.png'
+// @ts-ignore
+import p9 from '../../components/4-projects/p-9/p9.png'
+// @ts-ignore
+import p10 from '../../components/4-projects/p-10/p10.png'
 
 
 import { useState } from 'react'
@@ -10,16 +30,16 @@ export default function Projects() {
 
     const [filterValue, setFilterValue] = useState(null); // State to hold the filter value
     const projects = [
-        { id: 1, name: 'landing page', img: '../../components/4-projects/P-1/p-1.png' , p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 2, name: 'landing page', img: '../../components/4-projects/p-2/p-2.png', p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 3, name: 'landing page', img: '../../components/4-projects/p-3/p-3.png', p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 4, name: 'landing page', img: '../../components/4-projects/p-4/p-4.png', p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 5, name: 'landing page', img: '../../components/4-projects/P-5/p-5.png', p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 6, name: 'portfolio page', img: '../../components/4-projects/p-6/p6.png', p: "static landing page is designed using React", src: "https://farah-ashraf.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/Farah-Ashraf', category: 'Category C' },
-        { id: 7, name: 'landing page', img: '../../components/4-projects/p-7/p-7.png', p: "static landing page is designed using Html, Css and Js", src: "https://d-sherif-abdelbaeth.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/D.Sherif.Abdelbaeth', category: 'Category B' },
-        { id: 8, name: 'landing page', img: '../../components/4-projects/p-8/p8.png', p: "static landing page is designed using Html, Css and Js", src: "https://spyro-gym-one.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/Spyro-Gym', category: 'Category B' },
-        { id: 9, name: 'landing page', img: '../../components/4-projects/p-9/p9.png', p: "static landing page is designed using Html, Css and Js", src: "https://responsive-bakery.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/responsive-bakery', category: 'Category B' },
-        { id: 10, name: 'landing page', img: '../../components/4-projects/p-10/p10.png', p: "static landing page is designed using Html, Css and Js", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/BSA-Form', category: 'Category B' },
+        { id: 1, name: 'landing page', img: `${p1}` , p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
+        { id: 2, name: 'landing page', img: `${p2}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
+        { id: 3, name: 'landing page', img: `${p3}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
+        { id: 4, name: 'landing page', img: `${p4}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
+        { id: 5, name: 'landing page', img: `${p5}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
+        { id: 6, name: 'portfolio page', img: `${p6}`, p: "static landing page is designed using React", src: "https://farah-ashraf.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/Farah-Ashraf', category: 'Category C' },
+        { id: 7, name: 'landing page', img: `${p7}`, p: "static landing page is designed using Html, Css and Js", src: "https://d-sherif-abdelbaeth.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/D.Sherif.Abdelbaeth', category: 'Category B' },
+        { id: 8, name: 'landing page', img: `${p8}`, p: "static landing page is designed using Html, Css and Js", src: "https://spyro-gym-one.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/Spyro-Gym', category: 'Category B' },
+        { id: 9, name: 'landing page', img: `${p9}`, p: "static landing page is designed using Html, Css and Js", src: "https://responsive-bakery.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/responsive-bakery', category: 'Category B' },
+        { id: 10, name: 'landing page', img: `${p10}`, p: "static landing page is designed using Html, Css and Js", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/BSA-Form', category: 'Category B' },
     ];
 
     // Function to handle button click and set the filter value
