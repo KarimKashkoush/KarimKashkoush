585b0dae5384d55258dbeb229678dd47dc793583
karimkashkoush <karimkashkoush5@gmail.com>
2024-04-08 19:56:13 +0200

    add

--- src/components/4-projects/Projects.jsx
+++ src/components/4-projects/Projects.jsx
@@ -21,6 +21,8 @@ import p8 from '../../components/4-projects/p-8/p8.png'
 import p9 from '../../components/4-projects/p-9/p9.png'
 // @ts-ignore
 import p10 from '../../components/4-projects/p-10/p10.png'
+// @ts-ignore
+import p11 from '../../components/4-projects/p-7/p-7.png'
 
 
 import { useState } from 'react'
@@ -30,16 +32,105 @@ export default function Projects() {
 
     const [filterValue, setFilterValue] = useState(null); // State to hold the filter value
     const projects = [
-        { id: 1, name: 'landing page', img: `${p1}` , p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 2, name: 'landing page', img: `${p2}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 3, name: 'landing page', img: `${p3}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 4, name: 'landing page', img: `${p4}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 5, name: 'landing page', img: `${p5}`, p: "static landing page is designed using Html and Css", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://leon-4zg4wkmj4-karimkashkoush.vercel.app/', category: 'Category A' },
-        { id: 6, name: 'portfolio page', img: `${p6}`, p: "static landing page is designed using React", src: "https://farah-ashraf.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/Farah-Ashraf', category: 'Category C' },
-        { id: 7, name: 'landing page', img: `${p7}`, p: "static landing page is designed using Html, Css and Js", src: "https://d-sherif-abdelbaeth.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/D.Sherif.Abdelbaeth', category: 'Category B' },
-        { id: 8, name: 'landing page', img: `${p8}`, p: "static landing page is designed using Html, Css and Js", src: "https://spyro-gym-one.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/Spyro-Gym', category: 'Category B' },
-        { id: 9, name: 'landing page', img: `${p9}`, p: "static landing page is designed using Html, Css and Js", src: "https://responsive-bakery.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/responsive-bakery', category: 'Category B' },
-        { id: 10, name: 'landing page', img: `${p10}`, p: "static landing page is designed using Html, Css and Js", src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/", codeSrc: 'https://github.com/KarimKashkoush/BSA-Form', category: 'Category B' },
+        {
+            id: 1,
+            name: 'landing page',
+            img: `${p1}`,
+            p: "static landing page is designed using Html and Css",
+            src: "https://leon-4zg4wkmj4-karimkashkoush.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/leon',
+            ategory: 'Category A'
+        },
+        {
+            id: 2,
+            name: 'landing page',
+            img: `${p2}`,
+            p: "static landing page is designed using Html and Css",
+            src: "https://karim-9qa946dkd-karimkashkoush.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/Karim',
+            category: 'Category A'
+        },
+        {
+            id: 3,
+            name: 'landing page',
+            img: `${p3}`,
+            p: "static landing page is designed using Html and Css",
+            src: "https://kasper-eta.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/Kasper',
+            category: 'Category A'
+        },
+        {
+            id: 4,
+            name: 'landing page',
+            img: `${p4}`,
+            p: "static landing page is designed using Html and Css",
+            src: "https://turbino-gym.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/Turbino-Gym',
+            category: 'Category A'
+        },
+        {
+            id: 5,
+            name: 'landing page',
+            img: `${p5}`,
+            p: "static landing page is designed using Html and Css",
+            src: "https://steller-five.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/Steller',
+            category: 'Category A'
+        },
+        {
+            id: 6,
+            name: 'portfolio page',
+            img: `${p6}`,
+            p: "static landing page is designed using React",
+            src: "https://farah-ashraf.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/Farah-Ashraf',
+            category: 'Category C'
+        },
+        {
+            id: 7,
+            name: 'landing page',
+            img: `${p7}`,
+            p: "static landing page is designed using Html, Css and Js",
+            src: "https://d-sherif-abdelbaeth.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/D.Sherif.Abdelbaeth',
+            category: 'Category B'
+        },
+        {
+            id: 8,
+            name: 'landing page',
+            img: `${p8}`,
+            p: "static landing page is designed using Html, Css and Js",
+            src: "https://spyro-gym-one.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/Spyro-Gym',
+            category: 'Category B'
+        },
+        {
+            id: 9,
+            name: 'landing page',
+            img: `${p9}`,
+            p: "static landing page is designed using Html, Css and Js",
+            src: "https://responsive-bakery.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/responsive-bakery',
+            category: 'Category B'
+        },
+        {
+            id: 10,
+            name: 'landing page',
+            img: `${p10}`,
+            p: "static landing page is designed using Html, Css and Js",
+            src: "https://bsa-lake.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/BSA-Form',
+            category: 'Category B'
+        },
+        {
+            id: 11,
+            name: 'landing page',
+            img: `${p11}`,
+            p: "static landing page is designed using React",
+            src: "https://sherif.vercel.app/",
+            codeSrc: 'https://github.com/KarimKashkoush/sherif',
+            category: 'Category C'
+        },
     ];
 
     // Function to handle button click and set the filter value
@@ -59,20 +150,20 @@ export default function Projects() {
             <section className="content">
                 <section className="filter">
                     <button onClick={() => { setCurrentBtn('All Projects'); handleFilterClick(null) }} className={`btn ${currentBtn === "All Projects" ? "active" : null}`} >All Projects</button>
-                    <button onClick={() => { setCurrentBtn('Html & Css') ; handleFilterClick('Category A') }} className={`btn ${currentBtn === "Html & Css" ? "active" : null}`} >Html & Css</button>
-                    <button onClick={() => { setCurrentBtn('Html & Css & Js') ; handleFilterClick('Category B') }} className={`btn ${currentBtn === "Html & Css & Js" ? "active" : null}`} >Html & Css & Js</button>
-                    <button onClick={() => { setCurrentBtn('React & MUI') ; handleFilterClick('Category C') }} className={`btn ${currentBtn === "React & MUI" ? "active" : null}`} >React & MUI</button>
+                    <button onClick={() => { setCurrentBtn('Html & Css'); handleFilterClick('Category A') }} className={`btn ${currentBtn === "Html & Css" ? "active" : null}`} >Html & Css</button>
+                    <button onClick={() => { setCurrentBtn('Html & Css & Js'); handleFilterClick('Category B') }} className={`btn ${currentBtn === "Html & Css & Js" ? "active" : null}`} >Html & Css & Js</button>
+                    <button onClick={() => { setCurrentBtn('React & MUI'); handleFilterClick('Category C') }} className={`btn ${currentBtn === "React & MUI" ? "active" : null}`} >React & MUI</button>
                 </section>
                 <section className="all-projects">
                     {filteredProjects.map((project) => (
-                        <a key={project.id} className='project' href= {project.src}>
-                            <img src = {project.img} />
+                        <a key={project.id} className='project' href={project.src}>
+                            <img src={project.img} />
                             <section className="title">
                                 <h4>{project.name}</h4>
                                 <p>static landing page is designed using Html and Css</p>
                                 <section className="links">
                                     <Link to={project.src} className='icon-link1'></Link>
-                                    <Link to="https://github.com/KarimKashkoush/Leon" className='icon-github'></Link>
+                                    <Link to={project.codeSrc} className='icon-github'></Link>
                                 </section>
                             </section>
                         </a>
